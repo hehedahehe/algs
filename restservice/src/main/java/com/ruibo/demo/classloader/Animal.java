2f6cb470dba21467c0d73e43cce2f61b626f0af5
liruibo <liruibo@guazi.com>
2020-04-03 11:58:09 +0800

    +类加载机制

---
+++ restservice/src/main/java/com/ruibo/demo/classloader/Animal.java
@@ -0,0 +1,7 @@
+package com.ruibo.demo.classloader;
+
+public class Animal {
+	public void say() {
+		System.out.println("hello world!");
+	}
+}
--- restservice/src/main/java/com/ruibo/demo/classloader/ClassLoaderTest.java
+++ restservice/src/main/java/com/ruibo/demo/classloader/ClassLoaderTest.java
@@ -5,29 +5,37 @@ import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 
+/**
+ *
+ */
 public class ClassLoaderTest {
-
+	/**
+	 * @param args
+	 * @throws ClassNotFoundException
+	 * @throws IllegalAccessException
+	 * @throws InstantiationException
+	 */
 	public static void main(String[] args) throws ClassNotFoundException, IllegalAccessException, InstantiationException {
+		new ClassLoaderTest().test();
+	}
+
+
+	public void test() throws ClassNotFoundException, IllegalAccessException, InstantiationException {
 		//新建一个类加载器
 		MyClassLoader cl = new MyClassLoader("myClassLoader");
 		//加载类，得到Class对象
-		Class<?> clazz = cl.loadClass("classLoaderTest.Animal");
+		Class<?> clazz = cl.loadClass("com.ruibo.demo.classloader.Animal");
 		//得到类的实例
 		Animal animal = (Animal) clazz.newInstance();
 		animal.say();
 	}
 
-	class Animal {
-		public void say() {
-			System.out.println("hello world!");
-		}
-	}
 
-	static class MyClassLoader extends ClassLoader {
+	class MyClassLoader extends ClassLoader {
 		//类加载器的名称
 		private String name;
 		//类存放的路径
-		private String path = "E:\\workspace\\Algorithm\\src";
+		private String path = "/Users/liruibo/Documents/code/github/algs/restservice/target/classes/com/ruibo/demo/classloader/";
 
 		MyClassLoader(String name) {
 			this.name = name;
@@ -49,7 +57,6 @@ public class ClassLoaderTest {
 
 		public byte[] loadClassData(String name) {
 			try {
-				name = name.replace(".", "//");
 				FileInputStream is = new FileInputStream(new File(path + name + ".class"));
 				ByteArrayOutputStream baos = new ByteArrayOutputStream();
 				int b = 0;
