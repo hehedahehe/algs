f1fd3fdb5b6b229a151b8697d7121ae348e5d8e9
liruibo <l>
2020-04-06 21:37:11 +0800

    +zk+dubbo+sourcecode

---
+++ resigter-center-demo/src/test/java/com/ruibo/demo/registercenter/ResigterCenterDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ruibo.demo.registercenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResigterCenterDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
